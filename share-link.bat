c658049899580f3e72da31dd020f722ed8660d79
liquidboilerfuel <liquidboilerfuel@gmail.com>
2026-09-07 02:59:29 +0900

    Delete share-link.bat

--- share-link.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Creating a share link (run start.bat first and keep it open).
-echo Send the https://...trycloudflare.com URL to your friends.
-npx cloudflared tunnel --url http://localhost:3000
-pause
